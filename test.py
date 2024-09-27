492b187512e702826971c84c935a0ded073c05df
ShawnIsNotAwake <randyshih1201@gmail.com>
2024-09-26 20:11:39 -0700

    Change

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from tkinter import messagebox
+import google.generativeai as genai
+import os
+genai.configure(api_key="AIzaSyB_5KHGQ94m6cu_L-kEWeYzsXxuPmrvqp4")
 main = tk.Tk()
 main.geometry('800x600')
 print(dir(main))
 
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
+def askchatGPT(text):
+    response = model.generate_content(text)
+    textx.delete('1.0', END)
+    textx.insert('1.0', response.text)
+    return response.text
+
 
 def printe():
+    print(askchatGPT(entry.get()))
     if entry.get() == 'rawr':
         mainFrame.pack_forget()
         mainNOFRAME.pack(fill='both', expand=True)
@@ -15,7 +28,6 @@ def printe():
 def switchy():
     mainNOFRAME.pack_forget()
     mainFrame.pack(fill='both', expand=True)
-
 
 print('rawr')
 test2 = ttk.Notebook(main)
@@ -29,12 +41,16 @@ button.place(rely=.5, relx=.5, anchor='center')
 entry = tk.Entry(master=mainFrame)
 entry.place(rely=.55, relx=.5, anchor="center")
 mainFrame.pack(fill='both', expand=True)
+a = Scrollbar(master=main, orient='vertical')
+a.pack(fill='y', side=RIGHT)
+textx = Text(master=main, width=20, height=10, yscrollcommand=a.set)
+textx.place(relx=.5, rely=.3, anchor='center')
 n = ttk.Notebook(main)
-f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
-f2 = ttk.Frame(n)   # second page
+f1 = ttk.Frame(n)  # first page, which would get widgets gridded into it
+f2 = ttk.Frame(n)  # second page
 n.add(f1, text='One')
 n.add(f2, text='Two')
-n.place(relx=.1,rely=.2)
+n.place(relx=.1, rely=.2)
 
 main.resizable(height=False, width=False)
 main.mainloop()
